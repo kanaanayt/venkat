8402840be0741e7d6161c249fda84742feb06fa5
kanaanayt <kanaanabukhadra@gmail.com>
2024-04-19 19:08:38 +0300

    s

--- src/EMS.Api/Controllers/DepartmentsController.cs
+++ src/EMS.Api/Controllers/DepartmentsController.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using EMS.Api.BackendRepository;
 using EMS.Core;
 using Microsoft.AspNetCore.Mvc;
@@ -9,17 +10,41 @@ namespace EMS.Api.Controllers;
 public class DepartmentsController : ControllerBase
 {
     private readonly ICompanyRepository _repo;
+    private readonly IMapper _mapper;
     
-    public DepartmentsController(ICompanyRepository repo)
+    public DepartmentsController(ICompanyRepository repo, IMapper mapper)
     {
         _repo = repo;
+        _mapper = mapper;
     }
 
-    [HttpGet]
-    public async Task<IEnumerable<rDepartment>> GetDepartments()
+    [HttpGet(Name = "GetDept")]
+    public async Task<ActionResult<IEnumerable<rDepartment>>> GetDepartments()
     {
-        return await _repo.GetDepartmentsAsync();
+        var entities = await _repo.GetDepartmentsAsync();
+        return Ok(_mapper.Map<IEnumerable<rDepartment>>(entities));
     }
-    
-    
+
+    [HttpGet("id")]
+    public async Task<ActionResult<rDepartment>> GetDepartment(int id)
+    {
+        var entity = await _repo.GetDepartmentAsync(id);
+        return Ok(_mapper.Map<rDepartment>(entity));
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<rDepartment>> AddDepartment(cDepartment department)
+    {
+        var entity = _mapper.Map<eDepartment>(department);
+        await _repo.AddDepartmentAsync(entity);
+        var dto = _mapper.Map<rDepartment>(entity);
+        return CreatedAtRoute(
+            "GetDept",
+            new
+            {
+                Id = entity.Id,
+            },
+            dto);
+    }
+
 }
