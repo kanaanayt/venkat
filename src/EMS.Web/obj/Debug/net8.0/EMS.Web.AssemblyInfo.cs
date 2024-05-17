87479270d40217add51a9c590f29730a316f0828
kanaanayt <202101747@pmu.edu.sa>
2024-05-17 13:13:43 +0300

    a

--- src/EMS.Web/obj/Debug/net8.0/EMS.Web.AssemblyInfo.cs
+++ src/EMS.Web/obj/Debug/net8.0/EMS.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMS.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c212337bd49a7398d82be1e08d1316eda4501ded")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cae5791babe5d66ae471c8fbe03cbe4f64015d86")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMS.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMS.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
